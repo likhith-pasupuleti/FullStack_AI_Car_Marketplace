5ae9e50ef605e2d4c1ff84050643a67da2ff1874
likhith-pasupuleti <likhithsaikumarpasupuleti@gmail.com>
2026-03-02 10:37:13 -0600

    Added Reservations and Admin Layout Buttons

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import { SignedIn } from "@clerk/nextjs";
-import { CarFront } from "lucide-react";
+import { CarFront, Heart, Layout } from "lucide-react";
 import { Button } from "./ui/button";
 
 const Header = async ({ isAdminPage = false }) => {
@@ -26,12 +26,24 @@ const Header = async ({ isAdminPage = false }) => {
         {/*--SignedIn> is a conditional wrapper provided by Clerk.
         It renders its children only when the user is authenticated (logged in).
          If the user is not signed in, everything inside <SignedIn> is simply not rendered.*/}
-        <div>
+        <div className="flex items-center space-x-4">
           <SignedIn>
             <Link href="/saved-cars">
               <Button>
-                <CarFront size={18} />
+                <Heart size={18} />
                 <span className="hidden md:inline">Saved Cars</span>
+              </Button>
+            </Link>
+            <Link href="/reservations">
+              <Button variant="outline">
+                <CarFront size={18} />
+                <span className="hidden md:inline">My Reservations</span>
+              </Button>
+            </Link>
+            <Link href="/admin">
+              <Button variant="outline">
+                <Layout size={18} />
+                <span className="hidden md:inline">Admin Portal</span>
               </Button>
             </Link>
           </SignedIn>
